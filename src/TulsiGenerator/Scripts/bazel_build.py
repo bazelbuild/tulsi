43d057514b2d349d91fc18c5681e83afb9fe3118
Erik Abair <abaire@google.com>
2016-03-07 15:17:41 +0000

    Wires IPHONEOS_DEPLOYMENT_TARGET to Bazel's ios_minimum_os.
    --
    MOS_MIGRATED_REVID=116543663

--- src/TulsiGenerator/Scripts/bazel_build.py
+++ src/TulsiGenerator/Scripts/bazel_build.py
@@ -79,6 +79,11 @@ class _OptionsParser(object):
       self.build_options[_OptionsParser.ALL_CONFIGS].append(
           '--config=ios_' + arch)
 
+    ios_minimum_os = os.environ.get('IPHONEOS_DEPLOYMENT_TARGET', None)
+    if ios_minimum_os:
+      self.build_options[_OptionsParser.ALL_CONFIGS].append(
+          '--ios_minimum_os=' + ios_minimum_os)
+
     self.verbose = True
     self.install_generated_artifacts = False
 
