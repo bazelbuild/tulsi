9554f2119ec24c80bc07e6026f11ffae748bf168
Nicholas Levin <nglevin@google.com>
2017-09-19 21:45:58 +0000

    Make XcodeGeneratorInvalidPaths more flexible.
    --
    PiperOrigin-RevId: 169303562
    MOS_MIGRATED_REVID=169303562

--- src/TulsiGenerator/XcodeGeneratorInvalidPaths.swift
+++ src/TulsiGenerator/XcodeGeneratorInvalidPaths.swift
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-let invalidXcodeProjectPathsWithReasons = [
+var invalidXcodeProjectPathsWithReasons = [
   "/bazel-": "a Bazel generated temp directory",
 ]
