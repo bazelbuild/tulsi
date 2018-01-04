c490671da76f5af40428b1b774a603862084b32a
davg <davg@google.com>
2018-01-04 16:18:43 -0500

    Fix file permissions
    PiperOrigin-RevId: 180837192

--- src/Tulsi/BazelSelectionPanel.swift
+++ src/Tulsi/BazelSelectionPanel.swift
@@ -27,7 +27,7 @@ class BazelSelectionPanel: FilteredOpenPanel {
                                                           document: TulsiProjectDocument,
                                                           completionHandler: ((URL?) -> Void)? = nil) -> BazelSelectionPanel {
     let panel = BazelSelectionPanel()
-    panel.filterFunc = filterNonPackageDirectoriesOrFilesMatchingNames(["bazel", "blaze"])
+    panel.filterFunc = filterNonPackageDirectoriesOrFilesMatchingNames(["bazel", "bazel"])
     panel.delegate = panel
     panel.message = NSLocalizedString("ProjectEditor_SelectBazelPathMessage",
                                       comment: "Message to show at the top of the Bazel selector sheet, explaining what to do.")
