b014253ea5eaf46f032de2615d3d156e731ad14c
davg <davg@google.com>
2018-01-23 12:17:17 -0500

    Don't buffer build output
    Use a Bazel flag to prevent BEP output from being heavily buffered.
    
    PiperOrigin-RevId: 182943670

--- src/TulsiGenerator/Scripts/bazel_build.py
+++ src/TulsiGenerator/Scripts/bazel_build.py
@@ -777,6 +777,7 @@ class BazelBuildBridge(object):
 
     bazel_command.extend([
         '--build_event_json_file=%s' % self.build_events_file_path,
+        #  '--bes_outerr_buffer_size=0',  #  Waiting on a new external release.
         '--output_groups=tulsi-outputs,default',
         '--aspects', '@tulsi//tulsi:tulsi_aspects.bzl%tulsi_outputs_aspect',
         '--override_repository=tulsi=%s' % tulsi_package_dir,
