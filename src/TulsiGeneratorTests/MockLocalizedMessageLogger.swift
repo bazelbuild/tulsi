a5d3044197a59004aed1b3054d7bfeaec5b074a2
Nicholas Levin <nglevin@vivarium.gs>
2018-07-11 22:25:23 -0700

    Add non-fatal warning detection for mock logger.

--- src/TulsiGeneratorTests/MockLocalizedMessageLogger.swift
+++ src/TulsiGeneratorTests/MockLocalizedMessageLogger.swift
@@ -20,6 +20,10 @@ class MockLocalizedMessageLogger: LocalizedMessageLogger {
   var syslogMessages = [String]()
   var infoMessages = [String]()
   var warningMessageKeys = [String]()
+  let nonFatalWarningKeys = Set([
+    "BootstrapLLDBInitFailed",
+    "CleanCachedDsymsFailed"
+  ])
   var errorMessageKeys = [String]()
 
   init() {
@@ -50,7 +54,8 @@ class MockLocalizedMessageLogger: LocalizedMessageLogger {
   }
 
   func assertNoWarnings(_ file: StaticString = #file, line: UInt = #line) {
-    XCTAssert(warningMessageKeys.isEmpty,
+    let hasOnlyNonFatalWarnings = Set(warningMessageKeys).isSubset(of: nonFatalWarningKeys)
+    XCTAssert(warningMessageKeys.isEmpty || hasOnlyNonFatalWarnings,
               "Unexpected warning messages printed: \(warningMessageKeys)",
               file: file,
               line: line)
