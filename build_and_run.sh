dad7c85aa96707cf5903907b84a5c8358b516a73
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2020-06-09 17:14:32 -0400

    Additional parameter options for building Tulsi
    Allows users to specify where Tulsi should be unzipped to after install using the `-d` option and allows them to specify the Bazel path to use with the `-b` option
    
    Closes https://github.com/bazelbuild/tulsi/pull/151.
    
    PiperOrigin-RevId: 315557493

--- build_and_run.sh
+++ build_and_run.sh
@@ -21,11 +21,34 @@
 
 set -eu
 
-readonly unzip_dir="${1:-$HOME/Applications}"
+unzip_dir="$HOME/Applications"
+bazel_path="bazel"
+xcode_version="11.3.1"
+
+while getopts ":b:d:x:h" opt; do
+  case ${opt} in
+    h)
+      echo "Usage:"
+      echo "    ./build_and_run -h          Display this help message."
+      echo "    ./build_and_run -b PATH     Bazel binary used to build Tulsi"
+      echo "    ./build_and_run -d PATH     Intall Tulsi App at the provided path"
+      echo "    ./build_and_run -x VERSION  Xcode version Tulsi should be built for"
+      exit 0
+      ;;
+    b) bazel_path=$OPTARG;;
+    d) unzip_dir=$OPTARG;;
+    x) xcode_version=$OPTARG;;
+    ?)
+      echo "Invalid Option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
 
 # build it
-bazel build //:tulsi --use_top_level_targets_for_symlinks
+$bazel_path build //:tulsi --use_top_level_targets_for_symlinks --xcode_version=$xcode_version
 # unzip it
-unzip -oq $(bazel info workspace)/bazel-bin/tulsi.zip -d "$unzip_dir"
+unzip -oq $("$bazel_path" info workspace)/bazel-bin/tulsi.zip -d "$unzip_dir"
 # run it
 open "$unzip_dir/Tulsi.app"
