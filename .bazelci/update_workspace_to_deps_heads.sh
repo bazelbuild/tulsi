9007d8721e3f967bb6a5491b9a7c4337a113c41f
Keith Smiley <keithbsmiley@gmail.com>
2020-08-10 15:08:23 -0700

    Run CI in more configurations (#159)

---
+++ .bazelci/update_workspace_to_deps_heads.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Modify the WORKSPACE to pull in the master branches of some deps.
+/usr/bin/sed \
+  -i "" \
+  -e \
+    's/apple_rules_dependencies()/apple_rules_dependencies(ignore_version_differences = True)/' \
+  -e \
+    '1i \
+\
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")\
+\
+git_repository(\
+\    name = "bazel_skylib",\
+\    remote = "https://github.com/bazelbuild/bazel-skylib.git",\
+\    branch = "master",\
+)\
+\
+git_repository(\
+\    name = "build_bazel_apple_support",\
+\    remote = "https://github.com/bazelbuild/apple_support.git",\
+\    branch = "master",\
+)\
+\
+git_repository(\
+\    name = "build_bazel_rules_swift",\
+\    remote = "https://github.com/bazelbuild/rules_swift.git",\
+\    branch = "master",\
+)\
+' \
+  WORKSPACE
