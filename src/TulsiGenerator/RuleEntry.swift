25c601f7a3e2c238862facc03d7e6609c0e4b8fc
Googler <noreply@google.com>
2021-01-14 12:43:43 -0500

    Ignore the new Swift module map path.
    The Swift build rules are changing the path used for its generated
    module maps to fix a bug involving explicit modules.
    
    PiperOrigin-RevId: 351818306
    (cherry picked from commit cb9480b9a1dab0622e17ff7f64b673e99e1a9946)

--- src/TulsiGenerator/RuleEntry.swift
+++ src/TulsiGenerator/RuleEntry.swift
@@ -412,16 +412,15 @@ public final class RuleEntry: RuleInfo {
     // combine sources from potentially multiple targets into one test target.
     let targetsToAvoid = testDependencies + [label]
     let moduleMapsToAvoid = targetsToAvoid.compactMap { (targetLabel: BuildLabel) -> String? in
-      if let fileName = targetLabel.asFileName {
-        return "\(fileName).modulemaps/module.modulemap"
-      }
-      return nil
+      return targetLabel.asFileName
     }
     if !moduleMapsToAvoid.isEmpty {
       self.objCModuleMaps = objCModuleMaps.filter { moduleMapFileInfo in
         let moduleMapPath = moduleMapFileInfo.fullPath
         for mapToAvoid in moduleMapsToAvoid {
-          if moduleMapPath.hasSuffix(mapToAvoid) {
+          if moduleMapPath.hasSuffix("\(mapToAvoid).modulemaps/module.modulemap")
+            || moduleMapPath.hasSuffix("\(mapToAvoid).swift.modulemap")
+          {
             return false
           }
         }
