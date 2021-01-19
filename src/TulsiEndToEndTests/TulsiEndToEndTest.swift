c552d1b7ae31ce339cbb2abf783abdc631b3c7b4
davg <davg@google.com>
2021-01-19 15:13:48 -0500

    Update E2E tests to use iOS 14.2 simulators
    Xcode 12.2 (our current target) includes support for the iOS 14.2 simulator runtime.
    
    PiperOrigin-RevId: 352625026
    (cherry picked from commit 46db1d52097e5af6100df91add79b68fe165d110)

--- src/TulsiEndToEndTests/TulsiEndToEndTest.swift
+++ src/TulsiEndToEndTests/TulsiEndToEndTest.swift
@@ -21,7 +21,7 @@ import XCTest
 // generated xcodeproj by running the projects unit tests.
 class TulsiEndToEndTest: BazelIntegrationTestCase {
   fileprivate static let simulatorName = "tulsie2e-\(UUID().uuidString.prefix(8))"
-  fileprivate static let targetVersion = "13.3"
+  fileprivate static let targetVersion = "14.2"
 
   let fileManager = FileManager.default
   var runfilesWorkspaceURL: URL! = nil
