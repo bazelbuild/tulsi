55b6a9254546bb859fd782dd6f73a80993c1bcfc
ivanhernandez <ivanhernandez@google.com>
2021-06-10 16:20:20 +0000

    Change 3/3 to add a new tool that handles pruning the LLDB module cache of any modules that are found in a metadata file that is intended to be created by a bazel aspect that collects explicit module build outputs. This tool is needed to avoid a crash with LLDB that occurs when the LLDB module cache contains a module that is also generated in a build as an explicit module.
    To summarize how this works:
     - Read the contents of the implicit module cache and map the name of each module to the absolute path of that module.
     - Read metadata files that a bazel aspect will write to come up with a list of names of all explicit modules created from a build.
     - Iterate through the list of explicit module names and delete any module with the same name from the implicit module cache.
     - Compute a hash of the list of explicit module names that were used to prune the implicit module cache.
     - Save that hash to a special sentinel file in the implicit module cache.
     - Compare the hash of subsequent builds to the one in the file to determine if subsequent builds should prune the implicit module cache.
    
    PiperOrigin-RevId: 378666180

--- src/tools/module_cache_pruner/tests/FileUtils.swift
+++ src/tools/module_cache_pruner/tests/FileUtils.swift
@@ -76,3 +76,28 @@ func getTemporaryJSONFileURL() -> URL {
   let temporaryFilename = ProcessInfo().globallyUniqueString
   return temporaryDirectoryURL.appendingPathComponent("\(temporaryFilename).json")
 }
+
+/// Enumerates the full contents of a directory and returns a Set of the results containing only
+/// relative paths to the given directory.
+func getDirectoryContentsWithRelativePaths(directory: URL) -> Set<String> {
+  // The file manager enumerator function accepts an option that will enumerate with relative paths,
+  // i.e. FileManager.DirectoryEnumerationOptions.producesRelativePathURLs, but that options
+  // requires macOS 10.15 which is not guaranteed when building Tulsi. Instead, we create the
+  // relative paths ourselves.
+  guard let results = fileManager.enumerator(at: directory, includingPropertiesForKeys: nil) else {
+    return []
+  }
+
+  let directoryPath = directory.path
+  var relativeResults = Set<String>()
+  for result in results {
+    if let resultUrl = result as? URL {
+      let resultPath = resultUrl.path
+      let path =
+        resultPath.hasPrefix(directoryPath)
+        ? String(resultPath.dropFirst(directoryPath.count + 1)) : resultPath
+      relativeResults.insert(path)
+    }
+  }
+  return relativeResults
+}
