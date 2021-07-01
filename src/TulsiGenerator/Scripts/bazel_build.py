bff8f88527c1549a331a3fd313711c79f6a16a81
ivanhernandez <ivanhernandez@google.com>
2021-07-01 16:10:59 +0000

    Move usage of bazel_cache_reader behind a Tulsi option and leave it disabled by default. bazel_cache_reader has been observed to result in significant hits to symbolication in some cases.
    PiperOrigin-RevId: 382537207
    (cherry picked from commit 6fe2926e0b13bd240f5c2b7fa812032e06aed210)

--- src/TulsiGenerator/Scripts/bazel_build.py
+++ src/TulsiGenerator/Scripts/bazel_build.py
@@ -459,7 +459,9 @@ class BazelBuildBridge(object):
     self.direct_debug_prefix_map = False
     self.normalized_prefix_map = False
 
-    self.update_symbol_cache = UpdateSymbolCache()
+    self.update_symbol_cache = None
+    if os.environ.get('TULSI_USE_BAZEL_CACHE_READER') is not None:
+      self.update_symbol_cache = UpdateSymbolCache()
 
     # Path into which generated artifacts should be copied.
     self.built_products_dir = os.environ['BUILT_PRODUCTS_DIR']
@@ -1691,13 +1693,14 @@ class BazelBuildBridge(object):
       return False
 
     # Update the dSYM symbol cache with a reference to this dSYM bundle.
-    err_msg = self.update_symbol_cache.UpdateUUID(uuid,
-                                                  dsym_bundle_path,
-                                                  arch)
-    if err_msg:
-      _PrintXcodeWarning('Attempted to save (uuid, dsym_bundle_path, arch) '
-                         'to DBGShellCommands\' dSYM cache, but got error '
-                         '\"%s\".' % err_msg)
+    if self.update_symbol_cache is not None:
+      err_msg = self.update_symbol_cache.UpdateUUID(uuid,
+                                                    dsym_bundle_path,
+                                                    arch)
+      if err_msg:
+        _PrintXcodeWarning('Attempted to save (uuid, dsym_bundle_path, arch) '
+                           'to DBGShellCommands\' dSYM cache, but got error '
+                           '\"%s\".' % err_msg)
 
     return True
 
