4dd58989b34a948033869bfe4820edf52054580e
davg <davg@google.com>
2021-12-21 14:06:42 -0500

    Add UseLegacyBuildSystem option to swap between new/old build systems
    The new build system isn't officially supported yet, so I've added
    this flag in preparation for support although it's `true` (legacy)
    by default.
    
    This should supplement #277 although I haven't thoroughly tested the new build system support (especially on device + for tests) yet.
    
    PiperOrigin-RevId: 417654737
    (cherry picked from commit 2cbc9236818025b471689bd8a1810e6c0655a18a)

--- src/TulsiGenerator/TulsiOptionSet.swift
+++ src/TulsiGenerator/TulsiOptionSet.swift
@@ -80,6 +80,9 @@ public enum TulsiOptionKey: String {
       // i386.
       Use64BitWatchSimulator,
 
+      // Target the legacy build system instead of the new build system.
+      UseLegacyBuildSystem,
+
       // Option to fallback to using a global lldbinit.
       DisableCustomLLDBInit,
 
@@ -274,6 +277,13 @@ public class TulsiOptionSet: Equatable {
     return buildSettings
   }
 
+  // MARK: - Public Getters
+
+  /// Whether the legacy build system should be used instead of the new build system.
+  var useLegacyBuildSystem: Bool {
+    return self[.UseLegacyBuildSystem].commonValueAsBool ?? true
+  }
+
   // MARK: - Private methods.
 
   private func saveToDictionary(_ filter: (TulsiOptionKey, TulsiOption) -> Bool) -> PersistenceType {
@@ -341,6 +351,7 @@ public class TulsiOptionSet: Equatable {
     addBoolOption(.Use64BitWatchSimulator, .Generic, false)
     addBoolOption(.DisableCustomLLDBInit, .Generic, false)
     addBoolOption(.UseBazelCacheReader, .Generic, false)
+    addBoolOption(.UseLegacyBuildSystem, .Generic, true)
 
     let defaultIdentifier = PlatformConfiguration.defaultConfiguration.identifier
     let platformCPUIdentifiers = PlatformConfiguration.allValidConfigurations.map { $0.identifier }
