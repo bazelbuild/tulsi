b591ddb8eded73c931eb3a98d722d3c48ccf001b
davg <davg@google.com>
2022-02-01 10:57:54 -0500

    Fetch E2E build flags from the BazelIntegrationTestCase
    This avoids the needs to use a separate file for the build flags.
    
    Note the number of indexer targets changed since CppLib and ObjCLib
    are no longer built for iOS 10.
    
    PiperOrigin-RevId: 425623765
    (cherry picked from commit 9652ca7468416573b7b7f2a398e09963f24b0e8a)

--- src/TulsiEndToEndTests/ButtonsEndToEndTest.swift
+++ src/TulsiEndToEndTests/ButtonsEndToEndTest.swift
@@ -59,7 +59,7 @@ class ButtonsEndToEndTest: TulsiEndToEndTest {
       fileManager.fileExists(atPath: xcodeProjectURL.path), "Xcode project was not generated.")
     let targets = targetsOfXcodeProject(xcodeProjectURL)
     let indexTargets = targets.filter { $0.hasPrefix("_idx_") }
-    XCTAssertEqual(indexTargets.count, 8)
+    XCTAssertEqual(indexTargets.count, 6)
     for target in indexTargets {
       _ = buildXcodeTarget(xcodeProjectURL, target: target)
     }
